f471351426cfe5b64b1dbed4f40c18bf8f426f51
Tineil_A <tine6573@gmail.com>
2025-09-15 10:21:28 -0400

    created a webpage using react in lesson 2. About, Contact and Home pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <main className="p-10">
+            <h1 className="text-3xl">Contact Information</h1>
+            <p>Use this page to contact us</p>
+        </main>
+    );
+}
