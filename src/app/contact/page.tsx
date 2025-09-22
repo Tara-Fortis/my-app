60232c6359a0661ef877cb85c0a2639bc7e44120
Tineil_A <tine6573@gmail.com>
2025-09-22 10:48:36 -0400

    added message input and confrimation message

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,33 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Contact() {
+    const [name, setName] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
+    const [confirmation, setConfirmation] = useState<string>('');
+    // watch name input and and update state var as input value changes
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+    const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(event.target.value);
+    }
+
+    // show confrimation when button clicked
+    const handleSubmit = () => {
+        setConfirmation(`Your name: ${name} - Your message: ${message}`);
+    }
     return (
         <main>
             <h1>Contact Information</h1>
-            <p>Use this page to contact us</p>
+            <p>Contact us at XXX-XXX-XXXX or send us a message below</p>
+            <input type="text" placeholder="Your Name" value={name} onChange={handleNameChange} />
+            <p>Your name is: {name}</p>
+            <input type="text" placeholder="Your message" value={message} onChange={handleMessageChange} />
+            <p>Your message is: {message}</p>
+            <button onClick={handleSubmit}>Submit</button>
+            <p>{confirmation}</p>
         </main>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from "next/image";
+'use client';
+
+
+import { useState } from "react";
 
 export default function Home() {
+  // create a state var to track # of button clicks
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    // increment the count var by 1 each click
+    setCount(count + 1);
+  }
   return (
     // every React component must have 1 return to send the JSX to the DOM
-    <main className="p-10">
-      <h1 className="text-3xl">COMP2012 Class Site</h1>
+    <main>
+      <h1>COMP2012 Class Site</h1>
       <p>This site is built with Next.js</p>
+      <section>
+        <button onClick={handleClick}>Click Me</button>
+        <p>You clicked the button {count} times.</p>
+      </section>
     </main>
   )
 }
