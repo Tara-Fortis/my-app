f24891c444061a9a4e6759e17455df6a6353a2f9
Tineil_A <tine6573@gmail.com>
2025-10-06 11:10:47 -0400

    Added a submit button, corrected syntax errors

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -14,24 +14,26 @@ export default function Register() {
     const onSubmit = (data: PostFormData) => {
         console.log(`Account created: ${data}`);
     }
-    <main>
-        <h1>Create a new account</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <fieldset>
-                <label htmlFor="username">Username: *</label>
-                <input id='username' type='text'{...register("username", { required: "Username is required" })} />
-                {errors.username && <span className="error">{errors.username.message}</span>}
-            </fieldset>
-            <fieldset>
-                <label htmlFor="password">Password: *</label>
-                <input type="password" id="password" {...register("password", { required: "Password is required" })} />
-                {errors.password && <span className="error">{errors.password.message}</span>}
-            </fieldset>
-            <fieldset>
-                <label htmlFor="confirm">Confirm: *</label>
-                <input type="password" id="confirm" {...register("confirm", { required: "Confirm your password is required" })} />
-                {errors.confirm && <span className="error">{errors.confirm.message}</span>}
-            </fieldset>
-        </form>
-    </main>
+    return (
+        <main>
+            <h1>Create a new account</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset>
+                    <label htmlFor="username">Username: *</label>
+                    <input id='username' type='text' {...register("username", { required: "Username is required" })} />
+                    {errors.username && <span className="error">{errors.username.message}</span>}
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="password">Password: *</label>
+                    <input type="password" id="password" {...register("password", { required: "Password is required" })} />
+                    {errors.password && <span className="error">{errors.password.message}</span>}
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="confirm">Confirm: *</label>
+                    <input type="password" id="confirm" {...register("confirm", { required: "Confirm your password is required" })} />
+                    {errors.confirm && <span className="error">{errors.confirm.message}</span>}
+                </fieldset>
+                <button>Create account</button>
+            </form>
+        </main>)
 }
