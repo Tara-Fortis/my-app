237ee41ad482da82b6b53e0c5db2d183c8fe4d88
Tineil_A <tine6573@gmail.com>
2025-11-10 09:14:56 -0500

    Started api set up

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,37 +1,82 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form'
-import PageTitle from '@/app/components/PageTitle';
-import { useState } from 'react';
 
-interface LoginFormData {
+import { useForm } from "react-hook-form";
+import PageTitle from "@/app/components/PageTitle";
+import { useState } from "react";
+
+type LoginForm = {
     username: string;
     password: string;
 }
 
-
 export default function Login() {
-    const [message, setMessage] = useState<string>('Please enter your credentials');
 
-    const { register, handleSubmit, formState: { errors, isSubmitSuccessful } } = useForm<LoginFormData>();
+    //state variable with message to display to user
+    const [message, setMessage] = useState('Please enter your credentials')
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting }
+    } = useForm<LoginForm>();
 
+    const onSubmit = async (data: LoginForm) => {
+        //setMessage('Logging in...')
+        const apiDomain: string = process.env.NEXT_PUBLIC_API_DOMAIN!;
+
+        try {
+            const response: Response = await fetch(`${apiDomain}/users/login`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    username: data.username,
+                    password: data.password
+                })
+            });
+
+            // evaluate login attempt response
+            if (response.ok) {
+                console.log(response.json());
+            }
+            else {
+                setMessage('Invalid Login');
+                console.log(response.json());
+            }
+        }
+        catch (errors) {
+            console.log(`Login Error: ${errors}`);
+        }
+    };
 
     return (
         <main>
             <PageTitle title="Login" />
+
             <h1>Login</h1>
-            <p>{message}</p>
-            <form>
+
+            <div>{message}</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <fieldset>
-                    <label htmlFor="username">Username:*</label>
-                    <input type="text" id="username" {...register("username")} />
+                    <label>Username:</label>
+                    <input
+                        {...register("username", { required: "Username is required" })}
+                    />
+                    {errors.username && (
+                        <span className="error">{errors.username.message}</span>
+                    )}
                 </fieldset>
                 <fieldset>
-                    <label htmlFor="password">Password:*</label>
-                    <input type="password" id="password" {...register("password")} />
+                    <label>Password:</label>
+                    <input type="password"
+                        {...register("password", { required: "Password is required" })}
+                    />
+                    {errors.password && (
+                        <span className="error">{errors.password.message}</span>
+                    )}
                 </fieldset>
-                <button>Login</button>
+                <button className="btn" disabled={isSubmitting}>Login</button>
             </form>
         </main>
-    )
+    );
 }
+
