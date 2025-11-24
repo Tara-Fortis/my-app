08c8a6f44d89f4b219e3a8eac4fbcaec855f7636
Tineil_A <tine6573@gmail.com>
2025-11-24 10:39:10 -0500

    Lesson 11 part 2

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -9,8 +9,35 @@ interface RegisterFormData {
     confirm: string;
 }
 
+const getPasswordStrength = (password: string) => {
+    const checks = {
+        length: password.length >= 8,
+        lowercase: /[a-z]/.test(password),
+        uppercase: /[A-Z]/.test(password),
+        digit: /\d/.test(password),
+        special: /[^A-Za-z0-9]/.test(password)
+    };
+
+    const passed = Object.values(checks).filter(Boolean).length;
+
+    let label = 'Weak';
+    if (passed >= 3) label = 'Medium';
+    if (passed == 5) label = 'Strong';
+    return { label, passed, checks };
+}
+
 export default function Register() {
-    const { register, handleSubmit, formState: { errors, isSubmitSuccessful } } = useForm<RegisterFormData>();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors, isSubmitSuccessful }
+    } = useForm<RegisterFormData>();
+
+    // password strength check
+    const password = watch('password') || '';
+    const { label, checks } = getPasswordStrength(password);
+
 
     const onSubmit = async (data: RegisterFormData) => {
         if (data.password != data.confirm) {
@@ -51,7 +78,7 @@ export default function Register() {
             <PageTitle title="Register" />
             <h1>Create a new account</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <fieldset>
+                <fieldset className='password-fieldset'>
                     <label htmlFor="username">Username: *</label>
                     <input id='username' type='text' {...register("username", { required: "Username is required" })} />
                     {errors.username && <span className="error">{errors.username.message}</span>}
@@ -59,15 +86,33 @@ export default function Register() {
                 {/* Verify password === confirm */}
                 <fieldset>
                     <label htmlFor="password">Password: *</label>
-                    <input type="password" id="password" {...register("password", {
-                        required: "Password is required",
-                        minLength: {
-                            value: 8,
-                            message: "Min 8 characters"
-                        }
-                    }
-                    )} />
-                    {errors.password && <span className="error">{errors.password.message}</span>}
+                    <div className='password-row'>
+                        <div className='flex-1'>
+                            <input type="password" id="password" {...register("password", {
+                                required: "Password is required",
+                                minLength: {
+                                    value: 8,
+                                    message: "Min 8 characters"
+                                },
+                                pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]){8,}$/,
+                                    message: "Min 8 chars, 1 upper, 1 lower, 1 digit, 1 character"
+                                }
+                            }
+                            )} />
+                            {errors.password && (<span className="error">{errors.password.message}</span>
+                            )}
+                            <div className='strength-bar'>
+                                <div className={`strength-fill ${label === "Weak" ? "strength-weak"
+                                    : label === "Medium" ? "strenght-medium"
+                                        : "strength-strong"
+                                    }`}>
+
+                                </div>
+                            </div>
+                        </div>
+                        <p className="strength-label">Strength: {label}</p>
+                    </div>
                 </fieldset>
                 <fieldset>
                     <label htmlFor="confirm">Confirm: *</label>
